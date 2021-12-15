81e95f0e733bfe7dc04acb2d13cca969ca19f09a
Arogya Koirala <arogyakoirala@gmail.com>
2021-12-15 00:53:14 -0800

    updated configuration for client and apis

--- client/src/components/SearchContainer/index.api.js
+++ client/src/components/SearchContainer/index.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: 'http://api:8080',
+  baseURL: 'https://i202-api.arogyakoirala.com',
   headers: {
     'Content-Type': 'application/json',
   },
